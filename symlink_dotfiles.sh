0196b3fc0eefe7fe6ef42b96f687a55ebe6f4266
Charles Bedell <charlesbedell5@gmail.com>
2022-07-02 22:18:15 -0400

    added yabai skhd to symlink, ln now echoes creation on completion

--- symlink_dotfiles.sh
+++ symlink_dotfiles.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-for file in .zshrc .emacs
+for file in .zshrc .emacs .yabairc .skhdrc
 do
-		ln -s ~/dotfiles/"$file" ~/$file
-		echo "symlink created for $file in" ~/
-		
+  ln -s ~/dotfiles/"$file" ~/$file && echo "symlink created for $file in" ~/
 done
