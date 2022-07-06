7dd66d4143ee142c6265d5191f906a6931f0d683
Charles Bedell <charlesbedell5@gmail.com>
2022-07-05 23:23:36 -0400

    added other rcs to symlink script

--- symlink_dotfiles.sh
+++ symlink_dotfiles.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-for file in .zshrc .emacs .yabairc .skhdrc
+for file in .zshrc .emacs .yabairc .skhdrc .sketchybarrc 
 do
   ln -s ~/dotfiles/"$file" ~/$file && echo "symlink created for $file in" ~/
 done
